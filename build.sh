4bdac8f45e3f1c9624da2d23d35f1946f4665ceb
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-01-08 17:13:09 -0300

    updt build script to use pkg-config

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@
 # https://github.com/raysan5/raylib/wiki/Working-on-GNU-Linux
 # DOCS Cmd: $ cc main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
-RAYLIB_PATH=$HOME/software/raylib-4.5.0
-
 # Set modes
 #   -x (xtrace, trace mode, debug mode)
 #   -e (exit script if any command errors - exits with non-zero status)
@@ -16,13 +14,13 @@ set -xe
 # Define some C flags for the compiler
 #   -Wall (Enable all warnings)
 #   -Wextra (Enable more warnings no covered by -Wall)
-CFLAGS="-std=c99 -Wall -Wextra -I$RAYLIB_PATH/include
--I$RAYLIB_PATH/src/external/glfw/include/GLFW"
+CFLAGS="-Wall -Wextra `pkg-config --cflags raylib`"
 
-LIBS="-L$RAYLIB_PATH/lib -L$RAYLIB_PATH/examples/others/external/lib
--lraylib -lglfw -lm -ldl -lpthread"
+LIBS="`pkg-config --libs raylib` -lraylib -lglfw -lm -ldl -lpthread"
 
 SOURCE_FILE=main.c
 OUTPUT_FILE=musializer.out
 
 gcc $CFLAGS -o $OUTPUT_FILE $SOURCE_FILE $LIBS
+
+echo -e "\n[SUCCESS] Compiled projects successfully into $OUTPUT_FILE"
